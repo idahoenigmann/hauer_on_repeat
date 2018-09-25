3a240c4e029058e1bb7bcd389127dfe1a3eaafe7
Ida Hoenigmann <i15013@student.htlwrn.ac.at>
2018-09-25 11:20:25 +0200

    add torus.cpp and torus.h

---
+++ torus.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ida on 25.09.18.
+//
+
+#include "torus.h"
---
+++ torus.h
@@ -0,0 +1,8 @@
+//
+// Created by ida on 25.09.18.
+//
+
+#ifndef HAUER_ON_REPEAT_TORUS_H
+#define HAUER_ON_REPEAT_TORUS_H
+
+#endif //HAUER_ON_REPEAT_TORUS_H
