c90ac3c97ec8697920dd611cc57ff08f81b88a47
Manuel Eiwen <manueleiwen58@gmail.com>
2018-09-25 12:23:00 +0200

    add enum Pitch; add struct Node; begin implementation for Constructor

--- include/torus.h
+++ include/torus.h
@@ -5,9 +5,30 @@
 #ifndef HAUER_ON_REPEAT_TORUS_H
 #define HAUER_ON_REPEAT_TORUS_H
 
+enum Pitch {
+    low = 0,
+    med,
+    high
+};
+
+struct Node {
+    Node* up;
+    Node* left;
+    Node* down;
+    Node* right;
+
+    Pitch pitch;
+};
+
+
+
 class Torus {
   public:
     Torus();
+
+
+  private:
+    Node* start;
 };
 
 #endif //HAUER_ON_REPEAT_TORUS_H
--- src/torus.cpp
+++ src/torus.cpp
@@ -5,4 +5,29 @@
 #include <iostream>
 #include "torus.h"
 
-Torus::Torus() {}
+Torus::Torus() {
+
+    Node* firstNodeRowPtr;
+    Node* upperNodePtr;
+
+    //x and y as in coordinates
+    for (int y = 0; y < 4; y++) {
+        Node* firstNodePtr;
+        Node* lastNodePtr;
+        for (int x = 0; x < 12; x++) {
+            Node* curr = new Node();
+
+            if (x == 0) {
+                //set pointer for first node -> connection last and first node
+                firstNodePtr = curr;
+                if (y == 0) {
+                    //set pointer for very first node -> connection last and first row
+                    firstNodeRowPtr = curr;
+                }
+            }
+
+
+
+        }
+    }
+}
