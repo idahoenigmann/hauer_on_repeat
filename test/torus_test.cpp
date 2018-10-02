0f16f443040c15040917dc7d58918f2ced3a8e9f
Manuel Eiwen <manueleiwen58@gmail.com>
2018-10-02 12:30:24 +0200

    move CMake Files for tests out of git repo; add first test
    create test "test_nullptr"

--- test/torus_test.cpp
+++ test/torus_test.cpp
@@ -1,5 +1,39 @@
 #include "../src/torus.cpp"
+#include <cstdlib>
 #include <gtest/gtest.h>
+
+TEST(torus_test, test_nullptr) {
+    Torus torus = Torus();
+    Node* curr = torus.start;
+
+    for (int i = 0; i < 10000000; i++) {
+        int num = rand();
+
+        ASSERT_TRUE(curr->up != nullptr);
+        ASSERT_TRUE(curr->right != nullptr);
+        ASSERT_TRUE(curr->down != nullptr);
+        ASSERT_TRUE(curr->left != nullptr);
+
+        if (num % 4 == 0) {
+            curr = curr->up;
+        } else if (num % 4 == 1) {
+            curr = curr->down;
+        } else if (num % 4 == 2) {
+            curr = curr->left;
+        } else if (num % 4 == 3) {
+            curr = curr->right;
+        }
+    }
+}
+
+TEST(torus_test, test_continuity) {
+
+}
+
+TEST(torus_test, test_constructor) {
+
+}
+
 
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
