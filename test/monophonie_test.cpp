681682c2bc6f93d5c69ea2f67ad6953e5b6ef9ad
Manuel Eiwen <manueleiwen58@gmail.com>
2018-11-13 11:52:13 +0100

    add another monophonie (fixed) test

--- test/monophonie_test.cpp
+++ test/monophonie_test.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 TEST(monophonie_test, test_create_monophonie) {
+    {
     int nums[12] {0, 4, 1, 5, 10, 11, 6, 2, 8, 9, 7, 3};
     Torus t = Torus(nums);
 
@@ -17,4 +18,19 @@ TEST(monophonie_test, test_create_monophonie) {
     vector<vector<int>> monophonie{create_monophonie(t.start, 0, false)};
 
     ASSERT_EQ(correct, monophonie);
+    }
+
+    {
+        int nums[12] = {7, 4, 0, 8, 1, 5, 10, 2, 6, 11, 9, 3};
+        Torus t = Torus(nums);
+
+        vector<vector<int>> correct {vector<int>{7, 3}, vector<int>{4,2}, vector<int>{0,4,7},
+                vector<int>{8,4,0}, vector<int>{1,4}, vector<int>{5,8,9}, vector<int>{10,8,5,1},
+                vector<int>{2,5,8}, vector<int>{6,10}, vector<int>{11}, vector<int>{9,6,5},
+                vector<int>{3,6}, vector<int>{7}};
+
+        vector<vector<int>> monophonie{create_monophonie(t.start, 0, false)};
+
+        ASSERT_EQ(correct, monophonie);
+    }
 }
