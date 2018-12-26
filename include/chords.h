206cf59ab61b8c15c862cb0f24206b71b234192a
Ida Hoenigmann <hoenigmann.ida@student.htlwrn.ac.at>
2018-12-26 10:29:04 +0100

    work on #45; add documentation to chords, notes and monophonie

--- include/chords.h
+++ include/chords.h
@@ -9,10 +9,42 @@
 #include "torus.h"
 #include "monophonie.h"
 
+//! creates chords, displays and plays them
+/*!
+ * @param start Node pointer to lowest note of first four chord
+ * @param shift difference between lowest note and c
+ * @param anschlussklang true if anschlussklang is needed; false otherwise
+ * @param midi false if chords should not be displayed and played; true otherwise
+ * @return vector representation of music piece
+ */
 std::vector<std::vector<int>> chords(Node* start, int shift, bool anschlussklang, bool midi=true);
+
+//! converts chords into a string in lilypond syntax
+/*!
+ * @param start Node pointer to lowest note of first four chord
+ * @param shift difference between lowest note and c
+ * @param anschlussklang true if anschlussklang is needed; false otherwise
+ * @return notes storing a string in lilypond syntax
+ */
 Notes create_chords(Node* start, int shift, bool anschlussklang);
 
+//! creates notes, displays and plays them
+/*!
+ * @param start Node pointer to lowest note of first four chord
+ * @param shift difference between lowest note and c
+ * @param anschlussklang true if anschlussklang is needed; false otherwise
+ * @param midi false if chords should not be displayed and played; true otherwise
+ * @return vector representation of music piece
+ */
 std::vector<std::vector<int>> notes(Node* start, int shift, bool anschlussklang, bool midi=true);
+
+//! converts notes into a string in lilypond syntax
+/*!
+ * @param start Node pointer to lowest note of first four chord
+ * @param shift difference between lowest note and c
+ * @param anschlussklang true if anschlussklang is needed; false otherwise
+ * @return notes storing a string in lilypond syntax
+ */
 Notes create_notes(Node* start, int shift, bool anschlussklang);
 
 #endif //HAUER_ON_REPEAT_CHORDS_H
--- include/monophonie.h
+++ include/monophonie.h
@@ -10,20 +10,48 @@
 #include <vector>
 #include "torus.h"
 
+//! Store music piece in lilypond syntax
 struct Notes {
-    std::string input;
-    std::vector<std::vector<int>> list;
+    std::string input;  //! string in lilypond syntax
+    std::vector<std::vector<int>> list; //! vector representation of music
 
+    //! constructor
+    /*!
+     * @param input string in lilypond syntax
+     * @param list vector representation of music
+     */
     Notes(std::string input, std::vector<std::vector<int>> list) {
         this->input = std::move(input);
         this->list = list;
     }
 };
 
+//! creates monophonie, displays and plays it
+/*!
+ * @param start Node pointer to lowest note of first four chord
+ * @param shift difference between lowest note and c
+ * @param anschlussklang true if anschlussklang is needed; false otherwise
+ * @param midi false if chords should not be displayed and played; true otherwise
+ * @return vector representation of music piece
+ */
 std::vector<std::vector<int>> monophonie(Node* start, int shift, bool anschlussklang, bool midi=true);
 
+//! converts monophonie into a string in lilypond syntax
+/*!
+ * @param start Node pointer to lowest note of first four chord
+ * @param shift difference between lowest note and c
+ * @param anschlussklang true if anschlussklang is needed; false otherwise
+ * @return notes storing a string in lilypond syntax
+ */
 Notes create_monophonie(Node* start, int shift, bool anschlussklang);
 
+//! playes monphonie and chords simultaneously
+/*!
+ * @param start Node pointer to lowest note of first four chord
+ * @param shift difference between lowest note and c
+ * @param anschlussklang true if anschlussklang is needed; false otherwise
+ * @param midi false if chords should not be displayed and played; true otherwise
+ */
 void monophonie_and_chords(Node* start, int shift, bool anschlussklang, bool midi=true);
 
 #endif //HAUER_ON_REPEAT_MONOPHONIE_H
