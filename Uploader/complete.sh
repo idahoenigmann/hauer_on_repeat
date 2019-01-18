40ea936b98cc04abbe4ca9d278fa07606a0b0e4b
Manuel Kubu <kubu.manuel@student.htlwrn.ac.at>
2019-01-18 08:15:51 +0100

    Change files.cpp, that the Uploader gets called after creating the song

--- Uploader/complete.sh
+++ Uploader/complete.sh
@@ -13,7 +13,7 @@ convert pic.jpg -extent 1920x1080 -background white pic.jpg
 ffmpeg -y -loop 1 -i pic.jpg -i ${1} -vcodec libx264 -vf 'scale=640:trunc(ow/a/2)*2' -shortest -acodec aac -strict experimental -vb 1024k -minrate 1024k -maxrate 1024k -bufsize 1024k -ar 44100 -vf format=yuv420p  output.mp4
 
 #upload the video
-java Test output.mp4
+java -classpath ../Uploader/ Test output.mp4
 
 #remove
 rm output.mp4
